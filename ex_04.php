d98ac21fbfa28a8892df6d91ccf845da4cda6da2
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-07-06 09:38:22 -0300

    feat: atividade 4 terminada

--- ex_04.php
+++ ex_04.php
@@ -10,8 +10,23 @@ $especiais = '!@#$%^&*()-_=+[]{}|;:,.<>?';
 
 $todosCaracter = $maisculas . $minusculas . $numeros . $especiais;
 $senha = '';
+
+
+
+    $ultimoIndice = strlen($todosCaracter) - 1; // aqui tiramos um pq o php começa a contar do 0
+
+    for ($i = 0; $i < $tamanho; $i++) {  //vai repetir o sorteio até a senha que eu pedir.
+
+     
+        $posicaoSorteada = random_int(0, $ultimoIndice); // aqui escolhe um caracter aleatorio em alguma posição
+        
+        
+        $senha .= $todosCaracter[$posicaoSorteada];  // Pega a letra que está nessa posição e joga na senha
+    }
+
+    return $senha;
 }
 
-echo gerarSenha = (16);
+echo  "SENHA ALEATORIA:  " .  gerarSenha(16);
 
 ?>
