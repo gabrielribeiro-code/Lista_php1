09b50e373f2bb649fb90e5bacbab79ff181fda55
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-07-06 09:23:38 -0300

    add - atividade 4

---
+++ ex_04.php
@@ -0,0 +1,17 @@
+<?php
+
+function gerarSenha($tamanho = 10) {
+
+
+$maisculas = 'ABCDEFGHIJKLMNOPUQXYZ';
+$minusculas = 'abcdefghiojklmnopuqxyz';
+$numeros = '0123456789';
+$especiais = '!@#$%^&*()-_=+[]{}|;:,.<>?';
+
+$todosCaracter = $maisculas . $minusculas . $numeros . $especiais;
+$senha = '';
+}
+
+echo gerarSenha = (16);
+
+?>
