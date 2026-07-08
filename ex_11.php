17901b6a3c1a1171d58413fc057a00756e7f70a7
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-07-08 09:05:32 -0300

    feat: alteraçoes nos comentarios da atividade 11.

--- ex_11.php
+++ ex_11.php
@@ -2,13 +2,13 @@
 
 function formatarTexto($texto){
 
-$maisculo = mb_strtoupper($texto, 'UTF-8');
+$maisculo = mb_strtoupper($texto, 'UTF-8'); // função do php que transforma tudo em maisculo
+ 
+$minusculo = mb_strtolower($texto, 'UTF-8'); // função do php que transforma tudo em minisculo
 
-$minusculo = mb_strtolower($texto, 'UTF-8');
+$pMaiscula = mb_convert_case($texto, MB_CASE_TITLE, 'UTF-8'); // função do php que transforma a primeira letra sempre em maisculo, com conceito de case.
 
-$pMaiscula = mb_convert_case($texto, MB_CASE_TITLE, 'UTF-8');
-
-$conta = mb_strlen($texto, 'UTF-8');
+$conta = mb_strlen($texto, 'UTF-8'); // função que conta a quantidade de letras.
 
 
 
