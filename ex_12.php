88dfde5c908d1d0a76ae0ac69a1850a3d5beee22
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-07-08 17:42:27 -0300

    feat: atividade 12 completa.

--- ex_12.php
+++ ex_12.php
@@ -2,31 +2,55 @@
 
 function analisarProdutos($produtos, $buscaProduto){
 
-
-$produtoCaro = $produtos[0];
-
-$produtoBarato = $produtos[0];
-
+$maiorPreco = 0;
+$menorPreco = 99999;
+$produtoCaro = "";
+$produtoBarato = "";
 $somaPrecos = 0;
+$encontrado = false;
 
-$mediaPreco 
+foreach ($produtos as $nome => $preco ){
 
-foreach ($produtos )
+if($preco > $maiorPreco) {
+$maiorPreco = $preco;
+$produtoCaro = $nome;
+}
 
+if($preco < $menorPreco){
+    $menorPreco = $preco;
+    $produtoBarato = $nome;
+}
 
+$somaPrecos = $somaPrecos + $preco;
 
-
-
-
-
-
+if($nome == $buscaProduto){
+    echo "Produto encontrado:  $nome - R$ $preco <br>";
+    $encontrado = true;
 
 }
 
+}
+$media = $somaPrecos / count($produtos);
 
+echo "Produto mais caro: $produtoCaro vale: R$ $maiorPreco <br>";
+echo "Produto mais barato: $produtoBarato vale: R$ $menorPreco <br>";
+echo "Media dos preços: R$ $media  <br>" ;
 
+if($encontrado == false){
+echo "Produto não encontrado";
 
-$produtos = [$nome, $preco];
+}
+}
+
+$produtos = [
+
+"Arroz" => 25,
+"Feijao" => 8,
+"Leite" => 6, 
+"Cafe" => 18
+];
+
+analisarProdutos($produtos, "Arroz");
 
 
 ?>
